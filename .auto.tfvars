44edfbd1dfa06852e29a3783077eb4041446f26f
‘josh <kingorijosphat@yahoo.com>
2022-09-04 13:48:05 -0400

    More changed

--- .auto.tfvars
+++ .auto.tfvars
@@ -2,3 +2,7 @@
 region = "us-east-2"
 
 environment = "Development"
+
+vpc_cidr = "10.0.0.0/16"  # 65536 private ip addresses
+
+public_subnet_1_cidr = "10.0.1.0/24" # 256 private ip addresses
--- variables.tf
+++ variables.tf
@@ -6,3 +6,12 @@ variable "region" {
 variable "environment" {
   default = "Development"
 }
+
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "public subnet 1 cidr"
+  
+}
