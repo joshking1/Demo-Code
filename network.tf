daca57e1598a546ef8733264eac9398528496647
‘josh <kingorijosphat@yahoo.com>
2022-09-04 14:16:07 -0400

    more changes

--- network.tf
+++ network.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "development-vpc" {
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.development-vpc.id}"
-  availability_zone = "${region}a"
+  availability_zone = "${var.region}a"
   tags = {
     Name = "${var.environment}-Public-Subnet-1"
   }
