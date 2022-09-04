83af4774a2b61e775b4f684d3576e20ecf775d78
‘josh <kingorijosphat@yahoo.com>
2022-09-04 12:52:25 -0400

    Demo Repository

--- .auto.tfvars
+++ .auto.tfvars
@@ -2,7 +2,3 @@
 region = "us-east-2"
 
 environment = "Development"
-
-vpc_cidr = "10.0.0.0/16"
-
-public_subnet_1_cidr  = "10.0.1.0/24"
--- variables.tf
+++ variables.tf
@@ -1,17 +1,8 @@
 
-
 variable "region" {
-  default = "us-east-2"
+  default = "us-east-2" 
 }
 
 variable "environment" {
   default = "Development"
-}
-
-variable "vpc_cidr" {
-  description = "VPC cidr block"
-}
-
-variable "public_subnet_1_cidr" {
-  description = "Public Subnet 1 cidr block"
 }
